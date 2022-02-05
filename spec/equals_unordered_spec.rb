c13ac5970088d3f02be2af766781dc3d628d0367
Kerry Buckley <kerryjbuckley@gmail.com>
2022-02-05 11:27:08 +0000

    Add more specs for hashes (which already worked)

--- spec/equals_unordered_spec.rb
+++ spec/equals_unordered_spec.rb
@@ -56,4 +56,44 @@ describe "equals_unordered matcher" do
   it "doesn't match deeply-nested arrays with different elements" do
     expect([1, [2, [3, 4], 5], 6]).not_to equal_unordered([[5, [4, 0], 2], 6, 1])
   end
+
+  it "matches deeply-nested hashes" do
+    expect({a: {b: 1, c: {d: 2, e: 3}}, f: 4}).to equal_unordered({f: 4, a: {c: {e: 3, d: 2}, b: 1}})
+  end
+
+  it "doesn't match deeply-nested hashes with different keys" do
+    expect({a: {b: 1, c: {d: 2, e: 3}}, f: 4}).not_to equal_unordered({f: 4, a: {c: {x: 3, d: 2}, b: 1}})
+  end
+
+  it "doesn't match deeply-nested hashes with different values" do
+    expect({a: {b: 1, c: {d: 2, e: 3}}, f: 4}).not_to equal_unordered({f: 4, a: {c: {e: 9, d: 2}, b: 1}})
+  end
+
+  it "matches deeply-nested shuffled mixtures of hashes and arrays" do
+    expect({
+      a: 1,
+      [42, :foo] => [1, :bar, [2, 3], {x: "foo", "bar" => [:foo, "bar"]}],
+      b: [1, 2, 3],
+      true => [10, [11, 12, 13], {a: {b: {c: 99, d: 100}}}]
+    }).to equal_unordered({
+      true => [[11, 13, 12], 10, {a: {b: {d: 100, c: 99}}}],
+      [:foo, 42] => [:bar, 1, [3, 2], {"bar" => ["bar", :foo], x: "foo"}],
+      a: 1,
+      b: [3, 1, 2]
+    })
+  end
+
+  it "doesn't match deeply-nested non-equivalent mixtures of hashes and arrays" do
+    expect({
+      a: 1,
+      [42, :foo] => [1, :bar, [2, 3], {x: "foo", "bar" => [:foo, "bar"]}],
+      b: [1, 2, 3],
+      true => [10, [11, 12, 13], {a: {b: {c: 99, d: 100}}}]
+    }).not_to equal_unordered({
+      true => [[11, 13, 12], 10, {a: {b: {d: 100, c: 99}}}],
+      [:foo, 42] => [:bar, 1, [3, 2], {"bar" => ["bar", :foo], x: "foo"}],
+      a: 1,
+      b: [3, 1, 4]
+    })
+  end
 end
